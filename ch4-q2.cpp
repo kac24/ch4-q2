1cc612f8702972c589db3b92a08ad8f0928c485d
Alec Chan <161157613+kac24@users.noreply.github.com>
2024-10-25 20:01:11 -0400

    Complete main function and add comments.

--- ch4-q2.cpp
+++ ch4-q2.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 
-double getValue(){
+//get a double value from user and return by value to main
+double getValue()
+{
 	std::cout << "Enter a double value: ";
 	double value{};
 	std::cin >> value;
 	return value;
 }
 
-char getSign() {
+//get a arithmetic sign from user and return by value to main
+char getSign() 
+{
 	std::cout << "Enter +, -, *. or /: ";
 	char sign{};
 	std::cin >> sign;
 	return sign;
 }
 
+/*get a double value from user twice, an arithmetic sign and display result if sign
+ * display result if sign matches one of 4 arithmetic signs.
+ * If sign is invalid, display nothing.
+ */
 int main()
 {
 	double a{ getValue() };
 	double b{ getValue() };
 	char sign{ getSign() };
-	if (sign == '+') {
+	if (sign == '+') 
+	{
 		std::cout << a << " + " << b << " is " << a + b;
+	}
+	else if (sign == '-')
+	{
+		std::cout << a << " - " << b << " is " << a - b;
+	}
+	else if (sign == '*')
+	{
+		std::cout << a << " * " << b << " is " << a * b;
+	}
+	else if (sign == '/')
+	{
+		std::cout << a << " / " << b << " is " << a / b;
 	}
 	return 0;
 }
